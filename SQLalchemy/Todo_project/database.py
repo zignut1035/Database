61b07680eac5ac8d2e132bb50bdc587f01b8ea6d
zignut1035 <treenut.yusufee@student.samk.fi>
2024-03-20 14:56:30 +0200

    TOdo SQLalchemy

--- SQLalchemy/Todo_project/database.py
+++ SQLalchemy/Todo_project/database.py
@@ -1,16 +1,27 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
-
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from datetime import datetime
 Base = declarative_base()
 engine = create_engine("sqlite:///todo.db", echo = False, future = True)
 
-#items
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    userName = Column(String, unique=True)
+    passWord = Column(String)
+    items = relationship("Item", back_populates="user")
+
 class Item(Base):
     __tablename__ = "items"
-    itemId = Column(Integer, primary_key = True, autoincrement = True)
+    itemId = Column(Integer, primary_key=True)
     name = Column(String)
+    description = Column(String)  
+    added_time = Column(DateTime, default=datetime.now)  
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="items")
 
 Base.metadata.create_all(engine)
 
-from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind = engine)
--- SQLalchemy/Todo_project/main.py
+++ SQLalchemy/Todo_project/main.py
@@ -1,8 +1,20 @@
 import sys
 import os
-from database import Item, Session
+from database import Item, User, Session
 
 def main():
+    function = input("Do you want to register or login?: ").lower()
+    if function not in ["register", "login"]:
+        print('Please type "register" or "login"')
+        sys.exit(1)
+    if function == 'register': 
+        register_user()
+    if function == 'login': 
+        logged_in_user = login_user()
+        if logged_in_user:
+            user_menu(logged_in_user)
+
+def user_menu(user):
     while True:
         print(f"{os.linesep}What do you want to do today?")
         print("1: View todo items")
@@ -10,41 +22,77 @@ def main():
         print("3: Remove item")
         print("4: Exit" + os.linesep)
         selection = input()
-        if selection == "1": showItems()
-        if selection == "2": createItem()
-        if selection == "3": removeItem()
-        if selection == "4": sys.exit("Goodbye")
+        if selection == "1": 
+            showItems(user)
+        if selection == "2": 
+            createItem(user)
+        if selection == "3": 
+            removeItem(user)
+        if selection == "4": 
+            sys.exit("Goodbye")
 
-def showItems():
+def register_user():
+    username = input("Type your username: ")
+    password = input("Type your password: ")
+    with Session() as session:
+        existing_user = session.query(User).filter_by(userName = username).first()
+        if existing_user:
+            print("Account already exists.")
+        else:
+            new_user = User(userName=username, passWord = password)
+            session.add(new_user)
+            session.commit()
+            print("User registered successfully.")
+            return new_user
+
+def login_user():
+    username = input("Type your username: ")
+    password = input("Type your password: ")
+    with Session() as session:
+        user = session.query(User).filter_by(userName = username, passWord = password).first()
+        if user:
+            print("Welcome to the system.")
+            return user
+        else:
+            print("Wrong username or password.")
+            return None
+
+def showItems(user):
     print("Your todo lists")
     print("___")
     with Session() as session:
-        items = session.query(Item)
+        items = session.query(Item).filter(Item.user_id == user.id)
         for item in items:
             itemId = item.itemId
             itemName = item.name
-            print(f"{itemId}: {itemName}")
-        print("___" + os.linesep)
+            description = item.description
+            added_time = item.added_time.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"Item ID: {itemId}")
+            print(f"Name: {itemName}")
+            print(f"Description: {description}")
+            print(f"Added Time: {added_time}")
+            print("___" + os.linesep)
 
-def createItem():
-    
+
+def createItem(user):
     print("Name for the item:")
     itemName = input()
+    description = input("Description (optional): ")
     with Session() as session:
-        newItem = Item( name = itemName )
+        newItem = Item(name=itemName, description=description, user_id=user.id)
         session.add(newItem)
         session.commit()
 
-def removeItem():
+def removeItem(user):
     with Session() as session:
-        itemAmount = session.query(Item).count()
+        itemAmount = session.query(Item).filter(Item.user_id == user.id).count()
         if itemAmount < 1:
             print("You should add some items first.")
             return
         print("Give ID to remove:")
         itemId = int(input())
-        removableItem = session.query(Item).filter( Item.itemId == itemId )
-        if (removableItem.count() > 0):
+        removableItem = session.query(Item).filter(Item.itemId == itemId, Item.user_id == user.id)
+        if removableItem.count() > 0:
             session.delete(removableItem.one())
             session.commit()
         else:
